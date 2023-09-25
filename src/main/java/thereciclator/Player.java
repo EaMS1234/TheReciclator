f6fbc39ca4e409d6f4009a5c6001049d725f1dae
Erick Augusto <52828716+EaMS1234@users.noreply.github.com>
2023-09-25 14:44:15 -0300

    Generic player controller subclass

---
+++ src/main/java/thereciclator/Player.java
@@ -0,0 +1,11 @@
+package thereciclator;
+
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.Pane;
+
+// Class that represents a player
+public class Player extends Character {
+    public Player(Pane container, ImageView sprite) {
+        super(container, sprite);
+    }
+}
